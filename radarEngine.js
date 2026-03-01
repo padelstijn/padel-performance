b687366542d3f779c3bb381b25e7c92036a724ea
padelstijn <padelstijn@gmail.com>
2026-03-01 02:56:46 +0100

    Add createRadar function for radar chart creation

--- radarEngine.js
+++ radarEngine.js
@@ -1,1 +1,19 @@
+export function createRadar(canvasId, labels, dataSet){
 
+new Chart(document.getElementById(canvasId), {
+type: 'radar',
+data: {
+labels: labels,
+datasets: [{
+label: "Performance",
+data: dataSet
+}]
+},
+options: {
+responsive: true,
+scales: {
+r: { min: 0, max: 100 }
+}
+}
+});
+}
