5cb48c7550b1022e50e1d0ea083383a7d786cf40
padelstijn <padelstijn@gmail.com>
2026-03-01 02:55:27 +0100

    Add score calculation and level determination functions

--- scoreEngine.js
+++ scoreEngine.js
@@ -1,1 +1,26 @@
+export function calculatePlayerScore(playerRatings) {
 
+let total = 0;
+let weightTotal = 0;
+
+for (let stroke in playerRatings) {
+
+let value = playerRatings[stroke].score;
+let weight = playerRatings[stroke].weight;
+
+total += value * weight;
+weightTotal += weight;
+}
+
+let result = (total / weightTotal) * 10;
+
+return Math.round(result);
+}
+
+export function determineLevel(score){
+
+if(score < 40) return "Beginner";
+if(score < 70) return "Gevorderd";
+return "Competitie";
+
+}
